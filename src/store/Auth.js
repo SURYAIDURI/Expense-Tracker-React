d676f3d2c873ad692a01196a4774cec490543b65
SURYA IDURI <tejs4100@gmail.com>
2023-01-30 09:09:20 +0530

    Auth Reducer

--- src/store/Auth.js
+++ src/store/Auth.js
@@ -1,20 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { useDispatch } from 'react-redux';
 
-const initialAuthState ={isAuthenticated : false}
+import classes from './Auth.module.css';
+import { authActions } from '../store/index';
 
-const authSlice = createSlice({
-    name : "authentication",
-    initialState : initialAuthState,
-    reducers :{
-        login(state){
-            state.isAuthenticated =true
-        },
-        logout(state){
-            state.isAuthenticated = false
-        }
-    }
-})
+const Auth = () => {
+  const dispatch = useDispatch();
 
-export const authAction = authSlice.actions
+  const loginHandler = (event) => {
+    event.preventDefault();
 
-export default authSlice.reducer
+    dispatch(authActions.login());
+  };
+
+  return (
+    <main className={classes.auth}>
+      <section>
+        <form onSubmit={loginHandler}>
+          <div className={classes.control}>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' />
+          </div>
+          <button>Login</button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default Auth;
